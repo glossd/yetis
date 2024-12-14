a2f6ec10694b0aeeca0c728594989c0f68851d4f
Dennis Gloss <dennisgloss23@gmail.com>
2024-12-15 00:45:08 +0100

    add ci

--- proxy/bin_test.go
+++ proxy/bin_test.go
@@ -3,5 +3,11 @@ package proxy
 import "testing"
 
 func TestExec(t *testing.T) {
-	// todo
+	pid, err := Exec(45678, 45679)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pid <= 0 {
+		t.Fatalf("got pid: %d", pid)
+	}
 }
